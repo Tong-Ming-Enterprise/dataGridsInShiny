6af004c8373183a7f44258d2c81ed3d60e3eb3f6
mark-laptop <linyoiang@gmail.com>
2023-04-24 12:59:16 +0800

    add checkbox

--- inst/aggrid/nonreactive/app.R
+++ inst/aggrid/nonreactive/app.R
@@ -6,7 +6,6 @@ addResourcePath("assets", system.file("aggrid","nonreactive","assets", package =
 ui <- fluidPage(
 	tags$head(tags$script(src= "assets/this_app.js")),
 	actionButton("create_grid_mtcars", "Create ag-Grid with custom", class = "btn-primary"),
-	actionButton("create_grid_iris", "Create ag-Grid with iris", class = "btn-primary"),
 	tags$button(class = "btn btn-primary",
 				onClick = "sendGridData()",
 				"Send Data to DT"),
@@ -15,41 +14,38 @@ ui <- fluidPage(
 				style = 'visibility:hidden;',
 				"You have unsaved changes!"),
 	# textOutput("unsaved_warning"),
-	dataGridsInShiny::aggridUI(),
+	dataGridsInShiny::aggridUI('aggrid-container'),
 	DT::DTOutput("dt")
 
 )
 
 server <- function(input, output, session) {
+	rowData = list(
+		list( local_row_number = 1, po_row_id = 100, num_carton = 2, num_box = 2, num_bag = 0, num_piece = 1000, supplier_pallet = 1 ),
+		list( local_row_number = 2, po_row_id = 101, num_carton = 4, num_box = 0, num_bag = 0, num_piece = 2000, supplier_pallet = 1 ),
+		list( local_row_number = 3, po_row_id = 102, num_carton = 6, num_box = 0, num_bag = 0, num_piece = 3000, supplier_pallet = 2 ),
+		list( local_row_number = 4, po_row_id = 103, num_carton = 8, num_box = 0, num_bag = 0, num_piece = 4000, supplier_pallet = 2 ),
+		list( local_row_number = 5, po_row_id = 104, num_carton = 10, num_box = 0, num_bag = 0, num_piece = 5000, supplier_pallet = 3 ),
+		list( local_row_number = 6, po_row_id = 105, num_carton = 12, num_box = 0, num_bag = 48, num_piece = 6000, supplier_pallet = 5 )
+	)
+	columnDefs <- list(
+		list(headerName = "Row #", field = "local_row_number", checkboxSelection = TRUE, width = 80),
+		list(headerName = "PO", field = "po_row_id", width = 120),
+		list(headerName = "Carton", field = "num_carton", editable = TRUE, singleClickEdit = TRUE, width = 120),
+		list(headerName = "Box", field = "num_box", editable = TRUE, singleClickEdit = TRUE, width = 120),
+		list(headerName = "Bag", field = "num_bag", editable = TRUE, singleClickEdit = TRUE, width = 120),
+		list(headerName = "Piece", field = "num_piece", editable = TRUE, singleClickEdit = TRUE, width = 120),
+		list(headerName = "Pallet", field = "supplier_pallet", width = 120)
+	)
 	observe({
-		options <- list(rowHeaderLabelPrefix = "test ",
-						rowHeaderWidth = 300,
-						allowEditCells = FALSE)
-		#browser()
-		columnDefs <- list(
-			list(headerName = "Row #", field = "local_row_number", width = 80),
-			list(headerName = "PO", field = "po_row_id", width = 120),
-			list(headerName = "Carton", field = "num_carton", width = 120),
-			list(headerName = "Box", field = "num_box", width = 120),
-			list(headerName = "Bag", field = "num_bag", width = 120),
-			list(headerName = "Piece", field = "num_piece", width = 120),
-			list(headerName = "Pallet", field = "supplier_pallet", width = 120)
-		)
-		rowData = list(
-			list( local_row_number = 1, po_row_id = 100, num_carton = 2, num_box = 0, num_bag = 0, num_piece = 1000, supplier_pallet = 1 ),
-			list( local_row_number = 2, po_row_id = 101, num_carton = 4, num_box = 0, num_bag = 0, num_piece = 2000, supplier_pallet = 1 ),
-			list( local_row_number = 3, po_row_id = 102, num_carton = 6, num_box = 0, num_bag = 0, num_piece = 3000, supplier_pallet = 2 ),
-			list( local_row_number = 4, po_row_id = 103, num_carton = 8, num_box = 0, num_bag = 0, num_piece = 4000, supplier_pallet = 2 ),
-			list( local_row_number = 5, po_row_id = 104, num_carton = 10, num_box = 0, num_bag = 0, num_piece = 5000, supplier_pallet = 3 ),
-			list( local_row_number = 6, po_row_id = 105, num_carton = 12, num_box = 0, num_bag = 48, num_piece = 6000, supplier_pallet = 5 )
-		)
+
 		# Grid options
 		gridOptions <- list(
 			columnDefs = columnDefs,
 			rowData = rowData,
 			rowSelection = "multiple"
 		)
-
+		# send custom message to JS
 		session$sendCustomMessage(type = "create-aggrid",
 								  message = dataGridsInShiny::aggrid(gridOptions))}) %>%
 		bindEvent(input$create_grid_mtcars)
