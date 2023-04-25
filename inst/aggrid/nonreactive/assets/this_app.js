c6163252a4fb2add591edcdaae4814ad134b7e4b
mark-laptop <linyoiang@gmail.com>
2023-04-25 11:14:28 +0800

    clean up code

--- inst/aggrid/nonreactive/assets/this_app.js
+++ inst/aggrid/nonreactive/assets/this_app.js
@@ -1,21 +1,10 @@
 var aggrid
 var gridOptions
 
-//var columnDefs = []
-//var rowData = []
-// Grid options
-//var gridOptions2 = {
-//    columnDefs: columnDefs,
-//    rowData: rowData,
-//    rowSelection: "multiple",
-//    onCellValueChanged: onCellValueChanged, // Attach cell value changed event handler
-//    onRowSelected: onCellSelected
-//};
-
 // Define cell selection handler
 function onCellSelected(event) {
-  console.log("onCellSelected")
-  console.log(event)
+  //console.log("onCellSelected")
+  //console.log(event)
   //console.log(editedCell)
   // go thru each node and if its in editedRow select the row
   gridOptions.api.forEachNode(function(node) {
@@ -26,7 +15,8 @@ function onCellSelected(event) {
           node.setSelected(true)
       }
   });
-  // if checkbox selected we need to set it
+  console.log(event)
+  // if checkbox selected we need to set it. ??
   if (event.source == "checkboxSelected") {
     //console.log("checkboxSelected")
     editedRow.push(event.rowIndex)
@@ -37,19 +27,19 @@ function onCellSelected(event) {
 // this is where the AG-Grid is created in javascript
 // type must match calling .R session$sendCustomMessage(type = "create-aggrid",
 Shiny.addCustomMessageHandler(type = "create-aggrid", function(rgridOptions){
-	console.log(type)
+	//console.log(type)
 	const gridContainer = document.querySelector("#aggrid-container");
-	console.log("in custom message handler")
+	//console.log("in custom message handler")
 	// gridOptions pass infrom R
 	// adding javascript function event callback
 	gridOptions = rgridOptions
 	gridOptions.onCellValueChanged = onCellValueChanged
 	gridOptions.onRowSelected = onCellSelected
 
-	console.log(gridOptions)
+	//console.log(gridOptions)
 	//gridOptions.data = gridOptions.rowData;
 	aggrid = new agGrid.Grid(gridContainer, gridOptions);
-	console.log(aggrid)
+	//console.log(aggrid)
 });
 
 // Note: we should really use grid.events.on('change', sendChangeEvent), but even
@@ -66,20 +56,28 @@ function sendChangeEvent(gridEvent){
 	grid.events.off('setcellvalues', sendChangeEvent); // only need to send first event
 }
 
+// this function can be user to change edited cell type from char back to int
+function changeDataType(outdata) {
+	console.log(outdata)
+	return outdata
+}
 
 function sendGridData(){
-	console.log("sendGrdData")
-	console.log(gridOptions)
-	console.log(aggrid)
 	outData = []
 	gridOptions.api.forEachNode((rowNode, index) => {
 		outData.push(rowNode.data)
                     console.log('node ' + index + ' is in the grid');
                     console.log(rowNode.data)
     });
+
+    // ag-grid will change edited value to text this will cause issue when sending data back into R
+    // so need to change them to int. this will be case by case depend on what the origianl data is
+    outData = changeDataType(outData)
     console.log(outData)
 	// https://shiny.rstudio.com/articles/communicating-with-js.html
-	Shiny.setInputValue('griddata:aggrid.griddata', outData);
+	// https://book.javascript-for-r.com/shiny-complete.html
+	// variableName:inputHandlerName. variableName is griddata2 and inputHandler is defined in zzz.R call aggrid.griddata
+	Shiny.setInputValue('griddata2:aggrid.griddata', outData);
 	Shiny.setInputValue('unsaved_changes', false);
 
 	var unsaved_warning_button = document.getElementById("unsaved_warning_button");
@@ -102,88 +100,86 @@ const tableHeader = ["local_row_number", "po_row_id", "num_carton", "num_box", "
 
 // Create ag-Grid table
 const gridContainer = document.querySelector("#grid-container");
-//aggrid
-console.log("calling new here")
-//aggrid = new agGrid.Grid(gridContainer, gridOptions2);
-        // clear list after creation
-        // clear editedRow
-        editedRow = []
+
+// clear list after creation
+// clear editedRow
+editedRow = []
 
 
-        // Define cell value changed event handler
-        function onCellValueChanged(event) {
-          // Update the row data with the new value
-          //console.log("onCellValueChanged")
-          console.log(event)
-          editedRow.push(event.rowIndex)
-          //console.log("edited "+editedRow)
-          event.data[event.colDef.field] = event.newValue;
-          //console.log(event.colDef.field)
-          //editedCell[event.rowIndex][tableHeader.indexOf(event.colDef.field)] = 1
-          //console.log(editedCell)
+// Define cell value changed event handler
+function onCellValueChanged(event) {
+  // Update the row data with the new value
+  //console.log("onCellValueChanged")
+  //console.log(event)
+  editedRow.push(event.rowIndex)
+  //console.log("edited "+editedRow)
+  event.data[event.colDef.field] = event.newValue;
+  //console.log(event.colDef.field)
+  //editedCell[event.rowIndex][tableHeader.indexOf(event.colDef.field)] = 1
+  //console.log(editedCell)
 
-          // compare number aginst carton and pieces raise error if data is not valid
-          // bag needs to compare against carton
-          let errorFound = false
-          if (event.colDef.field == 'num_bag') {
-            if (Number(event.newValue) % event.data.num_carton != 0) {
-              errorFound = true
-              event.colDef.cellStyle = (p) =>
-                p.rowIndex.toString() === event.node.id ? {'background-color': errorBackgroundColor} : {};
+  // compare number aginst carton and pieces raise error if data is not valid
+  // bag needs to compare against carton
+  let errorFound = false
+  if (event.colDef.field == 'num_bag') {
+    if (Number(event.newValue) % event.data.num_carton != 0) {
+      errorFound = true
+      event.colDef.cellStyle = (p) =>
+        p.rowIndex.toString() === event.node.id ? {'background-color': errorBackgroundColor} : {};
 
-                event.api.refreshCells({
-                  force: true,
-                  columns: [event.column.getId()],
-                  rowNodes: [event.node]
-                });
-            }
-          }
-          if (event.colDef.field == 'num_box') {
-            if (Number(event.newValue) % event.data.num_carton != 0) {
-              errorFound = true
-              event.colDef.cellStyle = (p) =>
-                p.rowIndex.toString() === event.node.id ? {'background-color': errorBackgroundColor} : {};
+        event.api.refreshCells({
+          force: true,
+          columns: [event.column.getId()],
+          rowNodes: [event.node]
+        });
+    }
+  }
+  if (event.colDef.field == 'num_box') {
+    if (Number(event.newValue) % event.data.num_carton != 0) {
+      errorFound = true
+      event.colDef.cellStyle = (p) =>
+        p.rowIndex.toString() === event.node.id ? {'background-color': errorBackgroundColor} : {};
 
-                event.api.refreshCells({
-                  force: true,
-                  columns: [event.column.getId()],
-                  rowNodes: [event.node]
-                });
-            }
-          }
-          // https://stackoverflow.com/questions/62222534/ag-grid-change-cell-color-on-cell-value-change
-          if (!errorFound) {
-            if (event.oldValue !== event.newValue) {
-              event.colDef.cellStyle = (p) =>
-              p.rowIndex.toString() === event.node.id ? {'background-color': editedBackgroundColor} : {};
+        event.api.refreshCells({
+          force: true,
+          columns: [event.column.getId()],
+          rowNodes: [event.node]
+        });
+    }
+  }
+  // https://stackoverflow.com/questions/62222534/ag-grid-change-cell-color-on-cell-value-change
+  if (!errorFound) {
+    if (event.oldValue !== event.newValue) {
+      event.colDef.cellStyle = (p) =>
+      p.rowIndex.toString() === event.node.id ? {'background-color': editedBackgroundColor} : {};
 
-              event.api.refreshCells({
-                force: true,
-                columns: [event.column.getId()],
-                rowNodes: [event.node]
-              });
-            }
-          }
-        }
+      event.api.refreshCells({
+        force: true,
+        columns: [event.column.getId()],
+        rowNodes: [event.node]
+      });
+    }
+  }
+}
 
-        // handle output button click
-        function handleClick() {
-          console.log("exporting")
-          let items = [];
-          gridOptions.api.forEachNode(function(node) {
-              console.log(node.rowIndex)
-              if (editedRow.includes(node.rowIndex)) {
-                  console.log("edited"+node.rowIndex)
-                  items.push(node.data);
-              }
-          });
-          console.log(items)
-          //const data = gridOptions.api.getSelectedNodes(); // or api.getDataAsExcel() for Excel format
-          //console.log(data)
-          const data2 = gridOptions.api.getSelectedRows(); // or api.getDataAsExcel() for Excel format
-          console.log(data2)
-          // loop thru editedRow and create new data from it?
+// handle output button click
+function handleClick() {
+  console.log("exporting")
+  let items = [];
+  gridOptions.api.forEachNode(function(node) {
+      console.log(node.rowIndex)
+      if (editedRow.includes(node.rowIndex)) {
+          console.log("edited"+node.rowIndex)
+          items.push(node.data);
+      }
+  });
+  console.log(items)
+  //const data = gridOptions.api.getSelectedNodes(); // or api.getDataAsExcel() for Excel format
+  //console.log(data)
+  const data2 = gridOptions.api.getSelectedRows(); // or api.getDataAsExcel() for Excel format
+  console.log(data2)
+  // loop thru editedRow and create new data from it?
 
-          alert('Button clicked!');
-        }
+  alert('Button clicked!');
+}
 
