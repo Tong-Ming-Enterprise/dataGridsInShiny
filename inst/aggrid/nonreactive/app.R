3240020df28195157b9bd59a781e413ef2ae5c12
mark-laptop <linyoiang@gmail.com>
2023-04-27 09:07:42 +0800

    adding column with different data type

--- inst/aggrid/nonreactive/app.R
+++ inst/aggrid/nonreactive/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(tidyverse)
 devtools::load_all(".")
 
 addResourcePath("assets", system.file("aggrid","nonreactive","assets", package = "dataGridsInShiny"))
@@ -7,6 +8,7 @@ ui <- fluidPage(
 	tags$head(tags$script(src= "assets/this_app.js")),
 	# create_custom_aggrid is trigger in the server
 	actionButton("create_custom_aggrid", "Create custom ag-Grid", class = "btn-primary"),
+	actionButton("create_custom_aggrid2", "Create custom ag-Grid bad", class = "btn-primary"),
 	tags$button(class = "btn btn-primary",
 				onClick = "sendGridData()",       #this javascript function is found in this_app.js link in tag above
 				"Send Data to DT"),
@@ -16,33 +18,64 @@ ui <- fluidPage(
 				"You have unsaved changes!"),
 	# textOutput("unsaved_warning"),
 	dataGridsInShiny::aggridUI('aggrid-container'),
-	DT::DTOutput("dt")
-
+	DT::DTOutput("dt"),
+	DT::DTOutput("dtsel")
 )
+
+sampledf <- data.frame(
+	colId = c(1,2,3,4,5,6,7,8,9,10),
+	po_row_id = c(100, 101, 102, 103, 104, 105, 106, 107, 108, 109),
+	num_carton = c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20),
+	num_box = c(0, 0, 0, 0, 0, 0, 0, 16, 18, 0),
+	num_bag = c(0, 0, 0, 0, 0, 48, 28, 0, 0, 0),
+	num_piece = c(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000),
+	supplier_pallet = c(1, 1, 2, 2, 3, 5, 6, 8, 9, 11)
+)
+#https://www.geeksforgeeks.org/convert-dataframe-to-list-of-vectors-in-r/
+#list = as.list(sampledf)
 
 server <- function(input, output, session) {
 	rowData = list(
-		list( local_row_number = 1, po_row_id = 100, num_carton = 2, num_box = 2, num_bag = 0, num_piece = 1000, supplier_pallet = 1 ),
-		list( local_row_number = 2, po_row_id = 101, num_carton = 4, num_box = 0, num_bag = 0, num_piece = 2000, supplier_pallet = 1 ),
-		list( local_row_number = 3, po_row_id = 102, num_carton = 6, num_box = 0, num_bag = 0, num_piece = 3000, supplier_pallet = 2 ),
-		list( local_row_number = 4, po_row_id = 103, num_carton = 8, num_box = 0, num_bag = 0, num_piece = 4000, supplier_pallet = 2 ),
-		list( local_row_number = 5, po_row_id = 104, num_carton = 10, num_box = 0, num_bag = 0, num_piece = 5000, supplier_pallet = 3 ),
-		list( local_row_number = 6, po_row_id = 105, num_carton = 12, num_box = 0, num_bag = 48, num_piece = 6000, supplier_pallet = 5 )
+		list( local_row_number = 1, po_row_id = 100, num_carton = 2, num_box = 2, num_bag = 0, num_piece = 1000, supplier_pallet = 1, purhase_price = 10.0, done = TRUE, p_date = "2023-04-20", note = "1" ),
+		list( local_row_number = 2, po_row_id = 101, num_carton = 4, num_box = 0, num_bag = 0, num_piece = 2000, supplier_pallet = 1, purhase_price = 10.1, done = TRUE, p_date = "2023-04-20", note = "2" ),
+		list( local_row_number = 3, po_row_id = 102, num_carton = 6, num_box = 0, num_bag = 0, num_piece = 3000, supplier_pallet = 2, purhase_price = 20.2, done = FALSE, p_date = "2023-04-20", note = "1" ),
+		list( local_row_number = 4, po_row_id = 103, num_carton = 8, num_box = 0, num_bag = 0, num_piece = 4000, supplier_pallet = 2, purhase_price = 40.5, done = FALSE, p_date = "2023-04-20T06:00:00.000", note = "4" ),
+		list( local_row_number = 5, po_row_id = 104, num_carton = 10, num_box = 0, num_bag = 0, num_piece = 5000, supplier_pallet = 3, purhase_price = 50.9, done = TRUE, p_date = "2023-04-20T00:00:00.000", note = "1" ),
+		list( local_row_number = 6, po_row_id = 105, num_carton = 12, num_box = 0, num_bag = 48, num_piece = 6000, supplier_pallet = 5, purhase_price = 60.99, done = TRUE, p_date = "2023-04-20T23:00:00.000", note = "6" )
 	)
 
 	# type = 'numericColumn' still return char when cell has been edited MYL 04-25-2023
 
 	columnDefs <- list(
-		list(headerName = "Row #", field = "local_row_number", checkboxSelection = TRUE, width = 80),
+		list(headerName = "Row #", field = "local_row_number", checkboxSelection = TRUE, headerCheckboxSelection = TRUE, width = 110),
 		list(headerName = "PO", field = "po_row_id", width = 120),
-		list(headerName = "Carton", field = "num_carton", editable = TRUE, type = 'numericColumn', singleClickEdit = TRUE, width = 120),
+		list(headerName = "Carton", field = "num_carton", editable = TRUE, singleClickEdit = TRUE, width = 120),
 		list(headerName = "Box", field = "num_box", editable = TRUE, singleClickEdit = TRUE, width = 120),
 		list(headerName = "Bag", field = "num_bag", editable = TRUE, singleClickEdit = TRUE, width = 120),
 		list(headerName = "Piece", field = "num_piece", editable = TRUE, singleClickEdit = TRUE, width = 120),
-		list(headerName = "Pallet", field = "supplier_pallet", width = 120)
+		list(headerName = "Pallet", field = "supplier_pallet", width = 120),
+		list(headerName = "Price", field = 'purhase_price', cellClass = 'currencyFormat', editable = TRUE, singleClickEdit = TRUE, width =120),
+		list(headerName = "Done", field = "done", cellClass = 'booleanType', editable = TRUE, singleClickEdit = TRUE, width = 80),
+		list(headerName = "Date", field = 'p_date', cellClass = 'dateType', editable = TRUE, singleClickEdit = TRUE, width = 240),
+		list(headerName = "Select", field = "sel", editable = TRUE, singleClickEdit = TRUE, cellEditor = 'agSelectCellEditor', cellEditorParams = list(values = list('English', 'Spanish', 'French', 'Portuguese', '(other)'))),
+		list(headerName = "Note", field = "note", singleClickEdit = TRUE, cellEditor = 'agLargeTextCellEditor', cellEditorPopup = TRUE, cellEditorParams = list(maxLength = 100,rows = 10, cols = 50))
+
 	)
 	observe({
 
+		# Grid options
+		gridOptions <- list(
+			columnDefs = columnDefs,
+			rowData = rowData,
+			rowSelection = "multiple",
+			editType = 'fullRow'
+		)
+		# send custom message to JS.
+		# the columnDef, gridOptions and data are sent to javascript
+		session$sendCustomMessage(type = "create-aggrid-receiving",
+								  message = dataGridsInShiny::aggrid(gridOptions))}) %>%
+		bindEvent(input$create_custom_aggrid)
+	observe({
 		# Grid options
 		gridOptions <- list(
 			columnDefs = columnDefs,
@@ -53,8 +86,7 @@ server <- function(input, output, session) {
 		# the columnDef, gridOptions and data are sent to javascript
 		session$sendCustomMessage(type = "create-aggrid",
 								  message = dataGridsInShiny::aggrid(gridOptions))}) %>%
-		bindEvent(input$create_custom_aggrid)
-
+		bindEvent(input$create_custom_aggrid2)
 	observe({
 		options <- list(singleClickEdit = TRUE)
 		columnDefs <- list(
@@ -77,6 +109,9 @@ server <- function(input, output, session) {
 		req(input$griddata2)
 		input$griddata2})
 
+	output$dtsel <- DT::renderDT({
+		req(input$griddata3)
+		input$griddata3})
 	# output$unsaved_warning <- shiny::renderText({
 	# 	req(input$unsaved_changes)
 	# 	if(input$unsaved_changes)
